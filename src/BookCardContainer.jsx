47279960a94f711b56238e19583b62e2205a86f2
Aryan Tayal <bookwormshisar@gmail.com>
2026-08-03 19:56:19 +0530

    use uuid for book ids

--- src/BookCardContainer.jsx
+++ src/BookCardContainer.jsx
@@ -1,13 +1,18 @@
 import BookCard from "./BookCard";
+import { v4 as uuid } from "uuid";
+import "./styles/BookCardContainer.css";
 
-import "./styles/BookCardContainer.css"
-
-const BookCardContainer = ({bookData}) => {
+const BookCardContainer = ({ bookData }) => {
   return (
     <div className="BookCardContainer">
       {bookData.map((book) => {
-        if (book.sold !== true && book.issued !== true && book.active === true) {
-          return <BookCard key={book.id} {...book} />;
+        if (
+          book.sold !== true &&
+          book.issued !== true &&
+          book.active === true
+        ) {
+          const id = uuid();
+          return <BookCard key={id} {...book} id={id} />;
         }
       })}
     </div>
